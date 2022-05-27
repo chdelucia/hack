00558c33b34a43294dc7435430e24bcc0721c8b4
Christian Heredia <c.heredia87@gmail.com>
2022-05-27 14:58:44 +0200

    adding comments

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -21,6 +21,11 @@ export class BlogService {
     return blogDetails;
   }
 
+  /** No hagais esto NUNCA en casa, esto deberia ser un endpoint que se conecte a una base de datos
+   * El proposito de este blog es documentar mis aprendizajes
+   * Por eso priorizo la rapidez de montar una web sin tener que contratar un servicio online Basedatos
+   * ni tener que montar un backend con autentificacion
+   * */
   async getBlogHtmlbyId(fileName: string): Promise<string> {
     const code = await import(`../data/${fileName}`);
     return code?.blog || '';
